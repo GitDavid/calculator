1592811bdf4fab31928eba479233bfc2e7efacf0
David Liang <david.liang@nyu.edu>
2019-04-17 23:25:10 -0400

    basic logic sort of working?

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(a, b) {
-    return a + b;
+    return parseFloat(a) + parseFloat(b);
 }
 
 function subtract(a, b) {
@@ -18,29 +18,72 @@ function operate(a, operator, b) {
     switch (operator) {
         case "+": return add(a, b);
         case "-": return subtract(a, b);
-        case "*": return multiply(a, b);
-        case "/": return divide(a, b);
+        case "×": return multiply(a, b);
+        case "÷": return divide(a, b);
     }
 }
 
-const inputButtons = document.getElementsByClassName("input");
+const opInputButtons = document.getElementsByClassName("opinput");
+const numInputButtons = document.getElementsByClassName("numinput");
 const display = document.getElementById("display");
 const result = document.getElementById("result");
 const clear = document.getElementById("clear");
 const calculate = document.getElementById("calculate");
+const point = document.getElementById("point");
 
-Array.prototype.forEach.call(inputButtons, (button) => {
+let isStart = true;
+let displayEndsWith = "0";
+
+Array.prototype.forEach.call(numInputButtons, (button) => {
     button.addEventListener("click", () => {
+        if (isStart)
+            display.textContent = "";
         display.textContent += button.textContent;
+        isStart = false;
+        if (displayEndsWith == "op")
+            displayEndsWith = "num";
     })
 })
 
+Array.prototype.forEach.call(opInputButtons, (button) => {
+    button.addEventListener("click", () => {
+        if (isStart)
+            display.textContent = result.textContent;
+        if (displayEndsWith != "op")
+            display.textContent += button.textContent;
+        isStart = false;
+        displayEndsWith = "op"
+    })
+})
+
+point.addEventListener("click", () => {
+    if (displayEndsWith == "op") {
+        display.textContent += "0.";
+    } else if (displayEndsWith != ".") {
+        display.textContent += ".";
+    }
+    isStart = false;
+    displayEndsWith = ".";
+})
+
 clear.addEventListener("click", () => {
-    display.textContent = "";
+    result.textContent = "0";
+    display.textContent = "0";
+    displayEndsWith = "0";
+    isStart = true;
 })
 
 calculate.addEventListener("click", () => {
-    // assume proper formatting...for now
     const displayInput = display.textContent.split(" ");
-    result.textContent = operate(...displayInput);
+    const operators = ["×", "÷", "+", "-"];
+    for (let i = 0; i < operators.length; i++) {
+        const op = operators[i];
+        while (displayInput.includes(op)) {
+            const opIdx = displayInput.indexOf(op);
+            const opResult = operate(displayInput[opIdx - 1], op, displayInput[opIdx + 1]);
+            displayInput.splice(opIdx - 1, 3, opResult);
+        }
+    }
+    result.textContent = displayInput[0];
+    isStart = true;
 })
